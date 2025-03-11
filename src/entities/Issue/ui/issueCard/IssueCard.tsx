58a51e55d9741cf19620b595f393be565cd2fbc8
Maksym <maksimbondar225@gmail.com>
2025-03-11 19:37:52 +0100

    feat: dynamic IssueCard

--- src/entities/Issue/ui/issueCard/IssueCard.tsx
+++ src/entities/Issue/ui/issueCard/IssueCard.tsx
@@ -1,17 +1,22 @@
 import { Container, Card, CardBody } from "react-bootstrap";
 import styles from "./IssueCard.module.css";
+import {Issue} from "../../model/types.ts";
 
-const IssueCard = () => {
+interface IssueCardProps {
+    issue: Issue;
+}
+
+const IssueCard = ({issue}: IssueCardProps) => {
     return (
         <Container className="d-flex justify-content-center mt-3">
             <Card className={`${styles.card} shadow-sm rounded-3 w-100`} style={{ maxWidth: "500px" }}>
                 <CardBody className="d-flex flex-column gap-2 p-4">
-                    <p className="fw-bold fs-5 text-primary">Some issue title</p>
+                    <p className="fw-bold fs-5 text-primary">{issue.title}</p>
                     <p className="text-muted fs-6">
-                        <span className="fw-semibold">#315</span> opened 3 days ago
+                        <span className="fw-semibold">#315</span> {issue.createdAt}
                     </p>
                     <p className="text-muted fs-6">
-                        <span className="fw-semibold">Admin</span> | Comments: 11
+                        <span className="fw-semibold">{issue.author}</span> | Comments: {issue.commentsCount}
                     </p>
                 </CardBody>
             </Card>
