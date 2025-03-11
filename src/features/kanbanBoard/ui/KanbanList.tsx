e98c7761f2cd51adeb7d72d0c020cca00c427ec9
Maksym <maksimbondar225@gmail.com>
2025-03-11 20:30:33 +0100

    fix: fix KanbanList.tsx

--- src/features/kanbanBoard/ui/KanbanList.tsx
+++ src/features/kanbanBoard/ui/KanbanList.tsx
@@ -2,7 +2,6 @@ import {Issue, IssueState} from "../../../entities/Issue";
 import {Card, CardBody, Container} from "react-bootstrap";
 import IssueCard from "../../../entities/Issue/ui/issueCard/IssueCard.tsx";
 
-
 interface KanbanListProps {
     title: IssueState;
     items: Issue[];
@@ -12,8 +11,8 @@ const KanbanList = ({title, items}: KanbanListProps) => {
     return (
         <Container className="w-100">
             <p className="fw-semibold pb-2 text-center">{title}</p>
-            <Card className="shadow-sm rounded-1 p-3">
-                <CardBody className="d-flex flex-column gap-3 p-3 bg-secondary">
+            <Card className="shadow-sm rounded-1">
+                <CardBody className="d-flex flex-column gap-3 p-3 bg-secondary rounded-1">
                     {items.map((item: Issue) => <IssueCard issue={item}/>)}
                 </CardBody>
             </Card>
