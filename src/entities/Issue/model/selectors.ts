877860d593995e37d99a0f89515de1d774e00c69
Maksym <maksimbondar225@gmail.com>
2025-03-12 22:36:16 +0100

    feat: memoized issue selectors with reselect

--- src/entities/Issue/model/selectors.ts
+++ src/entities/Issue/model/selectors.ts
@@ -1,13 +1,24 @@
-import {RootState} from "../../../shared";
-import {Issue, IssueState} from "./types.ts";
+import { createSelector } from 'reselect';
+import { RootState } from "../../../shared";
+import { Issue, IssueState } from "./types.ts";
 
 export const selectAllIssues = (state: RootState): Issue[] => state.issueReducer.issues;
-export const selectToDoIssues = (state: RootState): Issue[] =>
-    state.issueReducer.issues.filter(issue => issue.state === IssueState.TODO);
-export const selectInProgressIssues = (state: RootState): Issue[] =>
-    state.issueReducer.issues.filter(issue => issue.state === IssueState.IN_PROGRESS);
-export const selectDoneIssues = (state: RootState): Issue[] =>
-    state.issueReducer.issues.filter(issue => issue.state === IssueState.DONE);
+
+export const selectToDoIssues = createSelector(
+    (state: RootState) => state.issueReducer.issues,
+    (issues) => issues.filter(issue => issue.state === IssueState.TODO)
+);
+
+export const selectInProgressIssues = createSelector(
+    (state: RootState) => state.issueReducer.issues,
+    (issues) => issues.filter(issue => issue.state === IssueState.IN_PROGRESS)
+);
+
+export const selectDoneIssues = createSelector(
+    (state: RootState) => state.issueReducer.issues,
+    (issues) => issues.filter(issue => issue.state === IssueState.DONE)
+);
+
 export const selectOpenIssuesLoading = (state: RootState) => state.issueReducer.isLoading_OpenIssues;
 export const selectInProgressIssuesLoading = (state: RootState) => state.issueReducer.isLoading_InProgressIssues;
 export const selectClosedIssuesLoading = (state: RootState) => state.issueReducer.isLoading_ClosedIssues;
