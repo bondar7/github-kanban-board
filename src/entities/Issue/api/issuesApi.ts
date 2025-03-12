71eb217d2de45f49d5bf94509f5974632fba0ae6
Maksym <maksimbondar225@gmail.com>
2025-03-12 22:08:53 +0100

    fix: re-create issuesApi to get open, open with assignee and closed issues.

--- src/entities/Issue/api/issuesApi.ts
+++ src/entities/Issue/api/issuesApi.ts
@@ -1,12 +1,74 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {Issue} from "../model/types.ts";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Issue, IssueState } from "../model/types.ts";
 
 export const issuesApi = createApi({
     reducerPath: "issuesApi",
     baseQuery: fetchBaseQuery({ baseUrl: "https://api.github.com" }),
     endpoints: (builder) => ({
-        getIssues: builder.query<Issue[], string>({
-            query: (repoFullName) => `/repos/${repoFullName}/issues`,
+        getOpenIssues: builder.query<Issue[], string>({
+            query: (repoFullName) => ({
+                url: `/repos/${repoFullName}/issues`,
+                params: {
+                    per_page: 100,
+                    page: 1,
+                    state: "open",
+                },
+            }),
+            transformResponse: (response: Issue[]): Issue[] => {
+                return response
+                    .map((issue: Issue) => ({
+                        ...issue,
+                        state: getState(issue),
+                    }))
+                    .filter((issue: Issue) => !issue.assignee);
+            },
         }),
-    })
-})
+        getInProgressIssues: builder.query<Issue[], string>({
+            query: (repoFullName) => ({
+                url: `/repos/${repoFullName}/issues`,
+                params: {
+                    per_page: 100,
+                    page: 1,
+                    state: "open"
+                },
+            }),
+            transformResponse: (response: Issue[]): Issue[] => {
+                return response
+                    .map((issue: Issue) => ({
+                        ...issue,
+                        state: getState(issue),
+                    }))
+                    .filter((issue: Issue) => issue.state === IssueState.IN_PROGRESS);
+            },
+        }),
+        getClosedIssues: builder.query<Issue[], string>({
+            query: (repoFullName) => ({
+                url: `/repos/${repoFullName}/issues`,
+                params: {
+                    per_page: 100,
+                    page: 1,
+                    state: "closed",
+                },
+            }),
+            transformResponse: (response: Issue[]): Issue[] => {
+                return response
+                    .map((issue: Issue) => ({
+                        ...issue,
+                        state: getState(issue),
+                    }));
+            },
+        }),
+    }),
+});
+
+const getState = (issue: Issue): IssueState => {
+    if (issue.state === "closed") {
+        return IssueState.DONE;
+    }
+
+    if (issue.assignee) {
+        return IssueState.IN_PROGRESS;
+    }
+
+    return IssueState.TODO;
+};
