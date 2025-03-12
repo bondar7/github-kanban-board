b81d9ab859137a4189bb01122172d2c135b31ac3
Maksym <maksimbondar225@gmail.com>
2025-03-12 23:00:12 +0100

    style: ui improves for IssueCard and KanbanBoard

--- src/entities/Issue/ui/issueCard/IssueCard.tsx
+++ src/entities/Issue/ui/issueCard/IssueCard.tsx
@@ -1,22 +1,30 @@
 import { Container, Card, CardBody } from "react-bootstrap";
 import styles from "./IssueCard.module.css";
-import {Issue} from "../../model/types.ts";
+import { Issue } from "../../model/types.ts";
+import getDaysAgo from "../../../../shared/utils/daysAgo.ts";
 
 interface IssueCardProps {
     issue: Issue;
 }
 
-const IssueCard = ({issue}: IssueCardProps) => {
+const IssueCard = ({ issue }: IssueCardProps) => {
+    const daysAgo = getDaysAgo(issue.created_at);
+
     return (
         <Container className="d-flex justify-content-center mt-3">
             <Card className={`${styles.card} shadow-sm rounded-3 w-100`} style={{ maxWidth: "500px" }}>
                 <CardBody className="d-flex flex-column gap-2 p-4">
-                    <p className="fw-bold fs-5 text-primary">{issue.title}</p>
+                    <p className={`${styles.cardTitle} fw-bold fs-5 text-primary`}>{issue.title}</p>
                     <p className="text-muted fs-6">
-                        <span className="fw-semibold">#315</span> {issue.createdAt}
+                        <span className="fw-semibold">#{issue.number}</span>
+                        {` opened ${daysAgo} ${daysAgo === "today" ? "" : daysAgo > 1 ? "days" : "day"} ${daysAgo === "today" ? "" : "ago"}`}
                     </p>
                     <p className="text-muted fs-6">
-                        <span className="fw-semibold">{issue.author}</span> | Comments: {issue.commentsCount}
+                        <span
+                            className="fw-semibold"
+                        >
+                            {typeof issue.assignee?.login === "string" ? issue.assignee.login : issue.user.login }
+                        </span> | Comments: {issue.comments}
                     </p>
                 </CardBody>
             </Card>
--- src/widgets/kanbanBoard/KanbanBoard.tsx
+++ src/widgets/kanbanBoard/KanbanBoard.tsx
@@ -1,6 +1,6 @@
-import {Issue, IssueState, selectDoneIssues, selectInProgressIssues, selectToDoIssues} from "../../entities/Issue";
+import { Issue, IssueState, selectDoneIssues, selectInProgressIssues, selectToDoIssues } from "../../entities/Issue";
 import useAppSelector from "../../shared/hooks/useAppSelector.ts";
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import KanbanList from "../../features/kanbanBoard/ui/KanbanList.tsx";
 
 const KanbanBoard = () => {
@@ -11,14 +11,14 @@ const KanbanBoard = () => {
     return (
         <Container>
             <Row>
-                <Col md={4}>
-                    <KanbanList title={"ToDo"} items={toDoIssues} type={IssueState.TODO}/>
+                <Col md={12} xs={12} sm={12} lg={4}>
+                    <KanbanList title={"ToDo"} items={toDoIssues} type={IssueState.TODO} />
                 </Col>
-                <Col md={4}>
-                    <KanbanList title={"In Progress"} items={inProgressIssues} type={IssueState.IN_PROGRESS}/>
+                <Col md={12} xs={12} sm={12} lg={4}>
+                    <KanbanList title={"In Progress"} items={inProgressIssues} type={IssueState.IN_PROGRESS} />
                 </Col>
-                <Col md={4}>
-                    <KanbanList title={"Done"} items={doneIssues} type={IssueState.DONE}/>
+                <Col md={12} xs={12} sm={12} lg={4}>
+                    <KanbanList title={"Done"} items={doneIssues} type={IssueState.DONE} />
                 </Col>
             </Row>
         </Container>
