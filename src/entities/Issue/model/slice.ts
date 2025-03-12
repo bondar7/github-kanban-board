2169bfd8a9c4024c9c96efd5d0f2c36c1a4cb3a7
Maksym <maksimbondar225@gmail.com>
2025-03-12 22:11:08 +0100

    fix: create extra-reducers to IssueSlice to save api responses into states

--- src/entities/Issue/model/slice.ts
+++ src/entities/Issue/model/slice.ts
@@ -12,22 +12,77 @@ const IssueSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addMatcher(issuesApi.endpoints.getIssues.matchPending, (state) => {
-            state.isLoading = true;
-            state.isError = false;
-            state.issues = [];
-        });
-        builder.addMatcher(issuesApi.endpoints.getIssues.matchFulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isError = false;
-            state.issues = action.payload;
-        });
-        builder.addMatcher(issuesApi.endpoints.getIssues.matchRejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.issues = [];
-        });
-    }
+        builder
+            // FULFILLED
+            .addMatcher(
+                issuesApi.endpoints.getOpenIssues.matchFulfilled,
+                (state, action) => {
+                    state.issues = [...state.issues, ...action.payload];
+                    state.isError_OpenIssues = false;
+                    state.isLoading_OpenIssues = false;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getInProgressIssues.matchFulfilled,
+                (state, action) => {
+                    state.issues = [...state.issues, ...action.payload];
+                    state.isError_InProgressIssues = false;
+                    state.isLoading_InProgressIssues = false;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getClosedIssues.matchFulfilled,
+                (state, action) => {
+                    state.issues = [...state.issues, ...action.payload];
+                    state.isError_ClosedIssues = false;
+                    state.isLoading_ClosedIssues = false;
+                }
+            )
+            // REJECTED
+            .addMatcher(
+                issuesApi.endpoints.getOpenIssues.matchRejected,
+                (state) => {
+                    state.isError_OpenIssues = true;
+                    state.isLoading_OpenIssues = false;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getInProgressIssues.matchRejected,
+                (state) => {
+                    state.isError_InProgressIssues = true;
+                    state.isLoading_InProgressIssues = false;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getClosedIssues.matchRejected,
+                (state) => {
+                    state.isError_ClosedIssues = true;
+                    state.isLoading_ClosedIssues = false;
+                }
+            )
+            // PENDING
+            .addMatcher(
+                issuesApi.endpoints.getOpenIssues.matchPending,
+                (state) => {
+                    state.isError_OpenIssues = false;
+                    state.isLoading_OpenIssues = true;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getInProgressIssues.matchPending,
+                (state) => {
+                    state.isError_InProgressIssues = false;
+                    state.isLoading_InProgressIssues = true;
+                }
+            )
+            .addMatcher(
+                issuesApi.endpoints.getClosedIssues.matchPending,
+                (state) => {
+                    state.isError_ClosedIssues = false;
+                    state.isLoading_ClosedIssues = true;
+                }
+            )
+    },
 });
 
 export const { setIssues } = IssueSlice.actions;
